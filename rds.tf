691ee3596c5af2c72a1e5407a9364830caa1fc9d
fawzy <fawzy@b.com>
2023-06-12 16:21:20 +0300

    add terraform state

--- rds.tf
+++ rds.tf
@@ -1,12 +1,11 @@
 resource "aws_db_instance" "default" {
-  allocated_storage    = 5
-  storage_type         = "gp2"
+  allocated_storage    = 10
+  db_name              = "mydb"
   engine               = "mysql"
   engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
-  db_name              = "mydb"
+  instance_class       = "db.t3.micro"
   username             = "foo"
-  password             = "${file("../passwords/rds.txt")}"
+  password             = file("../passwords/rds.txt")
   parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot = "true"
+  skip_final_snapshot  = true
 }
