ab11b2f44166dd3b2334859eb66801867dd016f3
fawzy <fawzy@b.com>
2023-06-12 15:07:10 +0300

    add rds

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
-resource "aws_instance" "web_dev" {
-  ami           = "ami-0715c1897453cabd1"
-  instance_type = "t2.micro"
+# resource "aws_instance" "web_dev" {
+#   ami           = "ami-0715c1897453cabd1"
+#   instance_type = "t2.micro"
 
 
-}
+# }
